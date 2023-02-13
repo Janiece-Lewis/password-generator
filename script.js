26fa7c3183f76a5f964c5a5eb8eca5a91c99cd16
Janiece-Lewis <jlewilewi@gmail.com>
2023-02-12 21:01:49 -0500

    added var for character type

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//added function to generate password
+
+function generatePassword() {
+  var passwordlength = prompt('How long would you like your password?')
+
+  //if statement for length of atleast 8 characters and no more than 128 characters
+  if (passwordlength < 8 || passwordlength > 128) {
+    alert('Your password must be between 8~128 characters')
+
+    //to exit function
+    return;
+  }
+
+  //to confirm character types
+  var uppercase = confirm("Do you want uppercase letters in your password?")
+  var lowercase = confirm("Do you want lowercase letters in your password?")
+  var numbers = confirm("Do you want numbers in your password?")
+  var specialCharacters = confirm("Do you want special characters in your password?")
+  console.log(numbers)
+
+  if (uppercase===true)
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
